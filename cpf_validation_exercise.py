2c9c574abf44d85550ed87c9d516b63c218c1d23
medinastr <pedromstracieri@gmail.com>
2024-03-11 13:25:05 -0300

    almost ready

--- cpf_validation_exercise.py
+++ cpf_validation_exercise.py
@@ -1,13 +1,20 @@
 # aux = input("Enter yout CPF: ")
 # cpf = int(aux)
-cpf = "223.450.312-93"
+cpf = "746.824.890-70"
 add = 0
 mult = 10
 
-for num in cpf:
+for num in cpf :
     if num.isdigit():
         add += int(num) * mult
+        #print(num, int(num) * mult, add)
         mult -= 1
+        if mult < 2 :
+            break
 
 add = (add * 10) % 11
-print(add)
+
+if add > 9 and cpf[0] == '0' or add == int(cpf[0]) :
+    print("CPF ok.")
+else :
+    print("Invalid CPF.")
