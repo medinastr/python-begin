92b48f3499f44612d1c4f403bce76e17291e7477
medinastr <pedromstracieri@gmail.com>
2024-03-18 23:50:35 -0300

    duplicate numbers just using lists

--- find_double.py
+++ find_double.py
@@ -1,11 +1,30 @@
-def find_double(l):
+"""
+find_first_duplicate(l) will receive a list of numbers, that will return -1 if are no
+duplicate numbers or the first duplicate number in list.
+The first duplicate is considered the second number that appears first.
+"""
+
+def find_first_duplicate(l):
+
+    smaller_index = len(l)
+    intact_list = l.copy()
+    found_numbers = []
+
     for ele1 in l:
-        l.remove(ele1)
-        if ele1 in l:
-            return ele1
+        lista_aux = l.copy()
+        lista_aux.remove(ele1)
 
-    return None
+        if ele1 in lista_aux:
+            i = lista_aux.index(ele1)
+
+            if i < smaller_index and ele1 not in found_numbers :
+                smaller_index = i
+            found_numbers.append(ele1)
+    
+    if smaller_index == len(l) :
+        return -1
+    return intact_list[smaller_index + 1]
 
 
-l = [1, 2, 3, 3, 2, 1, 0]
-print(find_double(l))
+l = [1, 3, 2, 10, 2, 3, 0]
+print(find_first_duplicate(l))
